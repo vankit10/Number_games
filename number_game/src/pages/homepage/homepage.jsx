e987519e5cba0b6987bba05ffb3a2d69a025045f
ANKIT VERMA <vankit10@gmail.com>
2023-04-12 21:14:22 +0530

    till 12 april

--- number_game/src/pages/homepage/homepage.jsx
+++ number_game/src/pages/homepage/homepage.jsx
@@ -1,17 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import './homepage.css';
 
 function Homepage(){
+    const [num,setNum]=useState(0)
+    const [flag,setFlag]=useState(false)
+    function increaseHandler(){
+        
+       if(num===10){
+        setFlag(!flag)
+       }
+       else{
+            setNum(num+1);
+            
+    }
+    }
+
+    function decreaseHandler(){
+        if(num===-10){
+            setFlag(!flag)
+        }
+
+            else{            
+            setNum(num-1);
+                 
+    }
+
+    }
+    function closeHandler(){
+        console.log("error")
+        setFlag(!flag)
+    }
+
     return (
         <div className="main-ctn">
-            <h2 className="number">0</h2>
+            <h2 className="number">{num}</h2>
             <div className="btn-ctn">
-                <button className="increase">INcrease</button>
-                <button className="decrease">DEcrease</button>
+                <button className="increase" onClick={increaseHandler}>INcrease</button>
+                <button className="decrease" onClick={decreaseHandler} >DEcrease</button>
+                {flag===true && <div className="warning-box">
+                    <button className="close" onClick={closeHandler}>X</button>
+                   
+                    <h2 className="warning">Number can not go beyond this</h2>
+                </div>}
             </div>
         </div>
     )
 }
+
 
 
 export default Homepage;
